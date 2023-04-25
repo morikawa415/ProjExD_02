4cf326ed2e804540251f74a82d7d4879e67b591e
Morikawa <c0b22148dc@edu.teu.ac.jp>
2023-04-25 15:50:23 +0900

    練習問題4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,8 @@ delta = {
     pg.K_RIGHT: [+1, 0]
     }
 
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -19,6 +21,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900,400
     tmr = 0
 
     bb_img = pg.Surface((20, 20))  # 練習１ 
@@ -29,6 +33,8 @@ def main():
     vx, vy = +1, +1
     bb_rct = bb_img.get_rect()
     bb_rct.center = x, y
+
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -38,10 +44,11 @@ def main():
 
 
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kk_rect.move_ip((0, -1))
+        for k, mv in delta.items():
+            if key_lst[k]:
+                kk_rct.move_ip(mv)
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
         #screen.blit(bb_img, [600, 200])
         bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
